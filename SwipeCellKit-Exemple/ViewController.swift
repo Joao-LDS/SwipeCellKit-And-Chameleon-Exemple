414cb7cef25d84cb8af2e1dc8090640d3a33ea3e
Joao-LDS <joao_l_d_s@hotmail.com>
2021-01-12 16:48:12 -0300

    First commit from local

---
+++ SwipeCellKit-Exemple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeCellKit-Exemple
+//
+//  Created by João Luis Santos on 12/01/21.
+//  Copyright © 2021 João Luis Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
