5c4607c038274d6c7c96835ec9630fe17d65e34a
Joao-LDS <joao_l_d_s@hotmail.com>
2021-01-13 12:20:27 -0300

    Install pod and swipecellkit, tableview exemple

--- SwipeCellKit-Exemple/ViewController.swift
+++ SwipeCellKit-Exemple/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import SwipeCellKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var data = ["Swipe", "Cell", "Kit", "Exemple"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.rowHeight = 80
     }
-
 
 }
 
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SwipeTableViewCell
+        cell.textLabel?.text = data[indexPath.row]
+        
+        cell.delegate = self
+        
+        return cell
+    }
+}
+
+extension ViewController: SwipeTableViewCellDelegate {
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Apagar") { action, indexPath in
+            self.data.remove(at: indexPath.row)
+        }
+        
+        deleteAction.image = UIImage(named: "trash")
+        
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        options.transitionStyle = .reveal
+        return options
+    }
+}
